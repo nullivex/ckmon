a6c3d3cadc1cb9c973668d14144c31275261a4dd
Bryan Tong <contact@nullivex.com>
2016-01-18 20:57:02 -0700

    use a better file slice method

--- main/routes/index.js
+++ main/routes/index.js
@@ -1,5 +1,5 @@
 'use strict';
-var cp = require('child_process')
+var sf = require('slice-file')
 
 var config = require('../../config')
 
@@ -10,7 +10,15 @@ var config = require('../../config')
  * @param {object} res
  */
 exports.index = function(req,res){
-  res.render('index',{
-    logContent: cp.execSync('tail -n 10 ' + config.ckpool.logFile)
+  var xs = sf(config.ckpool.logFIle)
+  xs.sliceReverse('-20',function(err,lines){
+    if(err){
+      console.log(err)
+      res.render('error',{error: err.msg})
+    } else{
+      res.render('index',{
+        logContent: lines.join('\n')
+      })
+    }
   })
 }
